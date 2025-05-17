2c0fc7653e30fcaf434d1be0782e552b1cf5aaec
Pavel Zinoviev <hello.pavel.zinoviev@gmail.com>
2025-05-17 22:13:10 +0400

    update

---
+++ src/data/site.js
@@ -0,0 +1,7 @@
+export const SITE_DATA = {
+  name: "yastaraus",
+  shortname: "y",
+  description: "A webzine about web development",
+  author: "Pavel Zinoviev",
+  keywords: ['webzine', 'personal', 'blog', 'pavel', 'zinoviev', 'webdev', 'development', 'html', 'css', 'javascript', 'js', 'lume', 'deno', 'web']
+}
